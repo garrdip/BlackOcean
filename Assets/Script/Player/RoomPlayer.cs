bd779a687b5e8f7e96e319a497a8319bad6ea913
BlackOrcDown <wnsvy223@naver.com>
2023-03-13 10:28:11 +0900

    1. 공통으로 사용할 수 있는 제네릭타입의 싱긍톤 클래스 추가 2. 캐릭터 선택 취소 추가
    Change-Id: I15b75ac8d6b13975898e12b4963a976f44e46a72

--- Assets/Script/Player/RoomPlayer.cs
+++ Assets/Script/Player/RoomPlayer.cs
@@ -12,34 +12,20 @@ public class RoomPlayer : NetworkRoomPlayer
     public Character character = Character.NONE;
 
 
-    // 로컬 플레이어로 시작 시 RoomUI의 레디 상태 변경 버튼이벤트 리스너 할당
+    // 로컬 플레이어로 시작 시 캐릭터 선택 델리게이트 연결 및 로컬플레이어 구분색 변경
     public override void OnStartLocalPlayer()
     {
         base.OnStartLocalPlayer();
-        if(RoomUI.instance != null){
-            RoomUI.instance.onCharacterSelect += OnCharacterSelected; // 캐릭터 선택 델리게이트 연결
-            transform.GetComponent<Image>().color = Color.yellow;
-        }
+        RoomUI.Instance.onCharacterSelect += OnCharacterSelected; 
+        transform.GetComponent<Image>().color = Color.yellow;
     }
 
     // 클라 + 호스트 시작 시 RoomPlayer오브젝트를 참가자 목록 레이아웃 하위로 설정해 리스트 되도록 세팅
     public override void OnStartClient()
     {
         base.OnStartClient();
-        if(RoomUI.instance != null){
-            transform.SetParent(RoomUI.instance.participantsLayout.transform);
-            transform.localScale = new Vector3(1, 1, 1);
-        }
-    }
-
-    // RoomPlayer오브젝트의 뷰 요소들 초기 세팅
-    private void InitRoomPlayerViewComponents()
-    {
-        transform.SetParent(RoomUI.instance.participantsLayout.transform); // 대기방 입장 시 RoomPlayer오브젝트를 참가자 패널UI 오브젝트의 하위오브젝트로 설정
-        TextMeshProUGUI selectedCharacterNameText = gameObject.transform.GetChild(1).GetChild(1).GetComponent<TextMeshProUGUI>();
-        selectedCharacterNameText.text = "Select Your Character";
-        TextMeshProUGUI buttonReadyText = gameObject.transform.GetChild(2).GetComponent<TextMeshProUGUI>();
-        buttonReadyText.text = "Idle";
+        transform.SetParent(RoomUI.Instance.participantsLayout.transform);
+        transform.localScale = new Vector3(1, 1, 1);
     }
 
     // RoomUI 클래스로부터 캐릭터 선택 델리게이트 이벤트를 수신하여, 선택한 캐릭터 정보 서버에 송신
@@ -50,27 +36,36 @@ public class RoomPlayer : NetworkRoomPlayer
         }
     }
 
-    private void SetDisableAlradySelectedCharacter(Character character)
+    // 선택된 캐릭터 뷰 컴포넌트 활성화, 비활성화 변경
+    private void ChangeSelectedCharacterActiveState(Character character)
     {
-        if(RoomUI.instance != null){
-           M_NetworkRoomManager M_NetworkRoomManager = NetworkRoomManager.singleton as M_NetworkRoomManager;
-            List<NetworkRoomPlayer> players = M_NetworkRoomManager.roomSlots;
-            foreach(RoomPlayer roomPlayer in players){
-                if(roomPlayer.character.Equals(character)){
-                    // TODO: 이벤트 수신 시 다른 플레이어가 선택한 캐릭터는 비활성화, 나머지는 활성화 되어 보이도록 해야함
-                }
+        // 매 이벤트 수신 시 모든 캐릭터 이미지, 버튼 활성화
+        RoomUI.Instance.characters[0].GetComponent<Image>().color = new Color(1f, 1f, 1f);
+        RoomUI.Instance.characters[1].GetComponent<Image>().color = new Color(1f, 1f, 1f);
+        RoomUI.Instance.characters[2].GetComponent<Image>().color = new Color(1f, 1f, 1f);
+        RoomUI.Instance.characters[0].GetComponent<Button>().interactable = true;
+        RoomUI.Instance.characters[1].GetComponent<Button>().interactable = true;
+        RoomUI.Instance.characters[2].GetComponent<Button>().interactable = true;
+        // 현재 방에 참가한 유저들이 선택한 캐릭터 이미지, 버튼은 비활성화
+        M_NetworkRoomManager M_NetworkRoomManager = NetworkRoomManager.singleton as M_NetworkRoomManager;
+        List<NetworkRoomPlayer> players = M_NetworkRoomManager.roomSlots;
+        foreach(RoomPlayer roomPlayer in players){
+            GameObject gameObject = RoomUI.Instance.characters.Find((obj) => obj.name.Equals(roomPlayer.character.ToString()));
+            if(gameObject != null){
+                gameObject.GetComponent<Image>().color = new Color(0.5f, 0.5f, 0.5f);
+                gameObject.GetComponent<Button>().interactable = false;
             }
         }
     }
 
-    // 부모 클래스인 NetworkRoomPlayer에 구현된 syncvar hook 가상함수를 ovveride하여 사용
+    // 부모 클래스인 NetworkRoomPlayer에 구현된 syncvar hook 가상함수를 override하여 사용
     public override void ReadyStateChanged(bool oldReadyState, bool newReadyState)
     {
         TextMeshProUGUI readyStateText = gameObject.transform.GetChild(2).GetComponent<TextMeshProUGUI>();
         if (newReadyState)
         {
             if(isLocalPlayer){
-                RoomUI.instance.buttonReady.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = Color.red;
+                RoomUI.Instance.buttonReady.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = Color.red;
             }
             readyStateText.color = Color.red;
             readyStateText.text = "Ready";
@@ -78,7 +73,7 @@ public class RoomPlayer : NetworkRoomPlayer
         else
         {
             if(isLocalPlayer){
-                RoomUI.instance.buttonReady.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = Color.black;
+                RoomUI.Instance.buttonReady.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = Color.black;
             }
             readyStateText.color = Color.black;
             readyStateText.text = "Idle";
@@ -87,7 +82,7 @@ public class RoomPlayer : NetworkRoomPlayer
 
     // 캐릭터 선택 이벤트 송신
     [Command]
-    private void CmdChangeCharacter(Character selectedCharacter)
+    public void CmdChangeCharacter(Character selectedCharacter)
     {
         character = selectedCharacter;
     }
@@ -98,9 +93,10 @@ public class RoomPlayer : NetworkRoomPlayer
         TextMeshProUGUI selectedCharacterNameText = gameObject.transform.GetChild(1).GetChild(1).GetComponent<TextMeshProUGUI>();
         selectedCharacterNameText.text = newCharacter.ToString();
         Debug.Log("선택된 캐릭터 :" + newCharacter);
-        SetDisableAlradySelectedCharacter(newCharacter);
+        ChangeSelectedCharacterActiveState(newCharacter);
     }
 
+    // 채팅 메시지 이벤트 송신
     [Command]
     public void CmdSend(string message, NetworkConnectionToClient sender = null)
     {
@@ -108,13 +104,12 @@ public class RoomPlayer : NetworkRoomPlayer
             RpcReceive("PlayerName", message.Trim());
     }
 
+    // 채팅 메시지 이벤트 수신
     [ClientRpc]
     void RpcReceive(string playerName, string message)
     {
         Debug.Log(playerName + "의 메시지 : "+ message);
-        if(RoomUI.instance != null){
-            RoomUI.instance.AppendMessage(playerName, message);
-        }
+        RoomUI.Instance.AppendMessage(playerName, message);
     }
-
 }
+
--- Assets/Script/UI/MenuUi.cs
+++ Assets/Script/UI/MenuUi.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MenuUi : MonoBehaviour
+public class MenuUI : MonoBehaviour
 {
     public Button buttonSinglePlay;
     public Button buttonMultiPlay;
