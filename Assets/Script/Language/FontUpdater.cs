3eb112c2b01dcb51c9c5b18635f8c148c0ee2942
garrdip <piryst@gmail.com>
2023-03-14 13:52:18 +0900

    한글 텍스트 추가 및 폰트 업데이트 스크립트 추가
    Change-Id: Ic7d1f00ec89d4477ba312d448652d854971f910d

---
+++ Assets/Script/Language/FontUpdater.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FontUpdater : MonoBehaviour
+{
+    public TextMeshProUGUI thisText;
+    void Start()
+    {
+        thisText = GetComponent<TextMeshProUGUI>();       
+        M_LanguageManager.languageChangedCallback += LanguageChanged;
+        StartCoroutine(nameof(InitialLanguageUpdate));
+    }
+
+    public void LanguageChanged()
+    {
+        thisText = GetComponent<TextMeshProUGUI>();
+        thisText.font = M_LanguageManager.currnetFont;
+    }
+
+    IEnumerator InitialLanguageUpdate()
+    {
+        while(M_LanguageManager.isLanguageLoadDone == false)
+        {
+            yield return new WaitForSeconds(0.1f);
+        }
+        LanguageChanged();
+    }
+}
