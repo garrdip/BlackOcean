a3fc271c5084663077059c3c67838c23a8802b81
garrdip <piryst@gmail.com>
2023-03-15 15:52:01 +0900

    Debug Console Added
    Change-Id: I8cbd6579c72fbc2dadc4b5d48515a33a069925ba

---
+++ Assets/Script/Console/Console.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Console : MonoBehaviour
+{
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
