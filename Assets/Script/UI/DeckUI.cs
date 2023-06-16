5fb70252f26f75ca51c83be0117a00e026c4a29e
wnsvy223 <wnsvy223@naver.com>
2023-06-16 10:55:04 +0900

    UI 클래스 분리
    Change-Id: If103642d61e07e7b77579acefb0a1f6a8da91289

---
+++ Assets/Script/UI/DeckUI.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+using DG.Tweening;
+using TMPro;
+using Steamworks;
+
+public class DeckUI : SingletonD<DeckUI>
+{
+    [Header("게임 오브젝트")]
+    public GameObject GameUI;
+    public GameObject GameBackGround;
+    public GameObject DeckListPanel;
+    public GameObject CardOnHandsPanel;
+
+
+    [Header("UI 컴포넌트")]
+    public Button buttonEndTurn;
+    public Button buttonPrefareDeck;
+    public Button buttonTrashDeck;
+    public Text textPrefareDeckCount;
+    public Text textTrashDeckCount;
+
+
+    [Header("플레이어 리스트(플레이어 정보 및 턴 정보)")]
+    public List<GameObject> playerOrderList;
+
+
+
+    // 턴 종료
+    public void HandleEndTurn()
+    {
+        NetworkClient.localPlayer.GetComponent<GamePlayer>().endTurnActive = !NetworkClient.localPlayer.GetComponent<GamePlayer>().endTurnActive;
+    }
+
+
+    // 댁 카운트 텍스트 컴포넌트들의 크기 변경 애니매이션(댁 카운트 변경 시 크기 커졌다 작아지는 애니매이션)
+    public void DeckCountTextScaleAnimation(Text textComponent, int count)
+    {
+        Vector3 chagenScale = new Vector3(2f, 2f, 2f);
+        Vector3 originScale = new Vector3(1f, 1f, 1f);
+        textComponent.text = count.ToString();
+        textComponent.transform.DOScale(chagenScale, 0.1f).SetEase(Ease.OutQuad)
+        .OnComplete(() =>
+        {
+            textComponent.transform.DOScale(originScale, 0.1f).SetEase(Ease.InQuad);
+        });
+    }
+}
--- Assets/Script/UI/PopUpUI.cs
+++ Assets/Script/UI/PopUpUI.cs
@@ -3,68 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Mirror;
-using DG.Tweening;
-using TMPro;
-using Steamworks;
 
-public class DeckUI : SingletonD<DeckUI>
+
+public class PopUpUI : SingletonD<PopUpUI>
 {
     [Header("게임 오브젝트")]
-    public GameObject GameBackGround;
-    public GameObject DeckListPanel;
-    public GameObject GameUI;
-    public GameObject CardOnHandsPanel;
-    public GameObject PrefareDeck;
-    public GameObject TrashDeck;
     public GameObject DeckListPopUp; // 덱 목록 팝업
     public GameObject DeckRemovePopUp; // 덱 제거 팝업
     public GameObject CardOnHandRemovePopUp; // 패 제거 팝업
     public GameObject LayoutCardOnHandForRemove;
+    public GameObject PrefareDeck;
+    public GameObject TrashDeck;
 
     [Header("UI에 사용되는 카드 프리팹")]
     public GameObject CardOnDeckPrefab;
 
-    [Header("UI 요소")]
-    public Button buttonEndTurn;
-    public Button buttonPrefareDeck;
-    public Button buttonTrashDeck;
-    public Button buttonReturnGame;
+
+    [Header("UI 컴포넌트")]
     public GridLayoutGroup deckListPopUpGrid;
     public GridLayoutGroup deckRemovePopUpGrid;
-    public Text textPrefareDeckCount;
-    public Text textTrashDeckCount;
     public Button buttonRemoveCardOnHandOk;
-    public Button buttonRemoveDeckOk;
-
+    public Button buttonReturnGame;
 
     [Header("댁 리스트")]
     public List<GameObject> deckList;
 
-    [Header("플레이어 리스트(플레이어 정보 및 턴 정보)")]
-    public List<GameObject> playerOrderList;
-
+    [Header("팝업 제어 변수")]
     private int originSiblingIndex = 0;
     private bool isOpenPrefareDeckPopUp = false;
     private bool isOpenTrashDeckPopUp = false;
     private bool isOpenCardOnHandRemovePopUp = false;
 
 
-    // 턴 넘김
-    public void HandleEndTurn()
-    {
-        NetworkClient.localPlayer.GetComponent<GamePlayer>().endTurnActive = !NetworkClient.localPlayer.GetComponent<GamePlayer>().endTurnActive;
-    }
 
     // PrefareDeck 정보 팝업
     public void HandleShowPrefareDeck()
     {
         isOpenPrefareDeckPopUp = !isOpenPrefareDeckPopUp; // 버튼 클릭으로도 팝업 열기 및 닫기
-        originSiblingIndex = buttonPrefareDeck.transform.GetSiblingIndex(); // 원래의 오브젝트 스택 순서 인덱스값
+        originSiblingIndex = DeckUI.instance.buttonPrefareDeck.transform.GetSiblingIndex(); // 원래의 오브젝트 스택 순서 인덱스값
         DeckListPopUp.gameObject.SetActive(isOpenPrefareDeckPopUp);
         if(isOpenPrefareDeckPopUp){
             // 팝업창 열리면 버튼 오브젝트의 부모를 팝업으로 바꾼뒤 가장 마지막 순서로 추가하여 팝업창 위에 버튼 그려지도록 변경
-            buttonPrefareDeck.transform.SetParent(DeckListPopUp.transform);
-            buttonPrefareDeck.transform.SetAsLastSibling();
+            DeckUI.instance.buttonPrefareDeck.transform.SetParent(DeckListPopUp.transform);
+            DeckUI.instance.buttonPrefareDeck.transform.SetAsLastSibling();
             M_CardManager.instance.ChangeCardOnHandSortingLayerByName("CardOnHand");
         }else{
             HandleReturnGame();
@@ -82,12 +63,12 @@ public class DeckUI : SingletonD<DeckUI>
     public void HandleShowTrashDeck()
     {
         isOpenTrashDeckPopUp = !isOpenTrashDeckPopUp; // 버튼 클릭으로도 팝업 열기 및 닫기
-        originSiblingIndex = buttonTrashDeck.transform.GetSiblingIndex(); // 원래의 오브젝트 스택 순서 인덱스값
+        originSiblingIndex = DeckUI.instance.buttonTrashDeck.transform.GetSiblingIndex(); // 원래의 오브젝트 스택 순서 인덱스값
         DeckListPopUp.gameObject.SetActive(isOpenTrashDeckPopUp);
         if(isOpenTrashDeckPopUp){
             // 팝업창 열리면 버튼 오브젝트의 부모를 팝업으로 바꾼뒤 가장 마지막 순서로 추가하여 팝업창 위에 버튼 그려지도록 변경
-            buttonTrashDeck.transform.SetParent(DeckListPopUp.transform);
-            buttonTrashDeck.transform.SetAsLastSibling();
+            DeckUI.instance.buttonTrashDeck.transform.SetParent(DeckListPopUp.transform);
+            DeckUI.instance.buttonTrashDeck.transform.SetAsLastSibling();
             M_CardManager.instance.ChangeCardOnHandSortingLayerByName("CardOnHand");
         }else{
             HandleReturnGame();
@@ -107,8 +88,11 @@ public class DeckUI : SingletonD<DeckUI>
         isOpenCardOnHandRemovePopUp = !isOpenCardOnHandRemovePopUp;
         CardOnHandRemovePopUp.gameObject.SetActive(isOpenCardOnHandRemovePopUp);
         if(isOpenCardOnHandRemovePopUp){
+            Button buttonPrefareDeck =  DeckUI.instance.buttonPrefareDeck;
             buttonPrefareDeck.transform.SetParent(CardOnHandRemovePopUp.transform);
             buttonPrefareDeck.transform.SetAsLastSibling();
+
+            Button buttonTrashDeck = DeckUI.instance.buttonTrashDeck;
             buttonTrashDeck.transform.SetParent(CardOnHandRemovePopUp.transform);
             buttonTrashDeck.transform.SetAsLastSibling();
             M_CardManager.instance.ChangeCardOnHandSortingLayerByName("CardOnHandOverPopUp");
@@ -122,8 +106,8 @@ public class DeckUI : SingletonD<DeckUI>
     {
         isOpenCardOnHandRemovePopUp = false;
         CardOnHandRemovePopUp.gameObject.SetActive(false);
-        buttonPrefareDeck.transform.SetParent(PrefareDeck.transform);
-        buttonTrashDeck.transform.SetParent(TrashDeck.transform);
+        DeckUI.instance.buttonPrefareDeck.transform.SetParent(PrefareDeck.transform);
+        DeckUI.instance.buttonTrashDeck.transform.SetParent(TrashDeck.transform);
         M_CardManager.instance.ChangeCardOnHandSortingLayerByName("CardOnHand");
     }
 
@@ -146,16 +130,16 @@ public class DeckUI : SingletonD<DeckUI>
         isOpenPrefareDeckPopUp = false;
         isOpenTrashDeckPopUp = false;
         if(isOpenCardOnHandRemovePopUp){
-            buttonPrefareDeck.transform.SetParent(CardOnHandRemovePopUp.transform);
-            buttonTrashDeck.transform.SetParent(CardOnHandRemovePopUp.transform);
+            DeckUI.instance.buttonPrefareDeck.transform.SetParent(CardOnHandRemovePopUp.transform);
+            DeckUI.instance.buttonTrashDeck.transform.SetParent(CardOnHandRemovePopUp.transform);
             M_CardManager.instance.ChangeCardOnHandSortingLayerByName("CardOnHandOverPopUp");
         }else{
-            buttonPrefareDeck.transform.SetParent(PrefareDeck.transform);
-            buttonTrashDeck.transform.SetParent(TrashDeck.transform);
+            DeckUI.instance.buttonPrefareDeck.transform.SetParent(PrefareDeck.transform);
+            DeckUI.instance.buttonTrashDeck.transform.SetParent(TrashDeck.transform);
             M_CardManager.instance.ChangeCardOnHandSortingLayerByName("CardOnHand");
         }
-        buttonPrefareDeck.transform.SetSiblingIndex(originSiblingIndex);
-        buttonTrashDeck.transform.SetSiblingIndex(originSiblingIndex);
+        DeckUI.instance.buttonPrefareDeck.transform.SetSiblingIndex(originSiblingIndex);
+        DeckUI.instance.buttonTrashDeck.transform.SetSiblingIndex(originSiblingIndex);
         DeckListPopUp.gameObject.SetActive(false);
         ClearDeckList();
     }
@@ -180,18 +164,5 @@ public class DeckUI : SingletonD<DeckUI>
             Destroy(deckList[i]);
             deckList.RemoveAt(i);
         }
-    }
-
-    // 댁 카운트 텍스트 컴포넌트들의 크기 변경 애니매이션(댁 카운트 변경 시 크기 커졌다 작아지는 애니매이션)
-    public void DeckCountTextScaleAnimation(Text textComponent, int count)
-    {
-        Vector3 chagenScale = new Vector3(2f, 2f, 2f);
-        Vector3 originScale = new Vector3(1f, 1f, 1f);
-        textComponent.text = count.ToString();
-        textComponent.transform.DOScale(chagenScale, 0.1f).SetEase(Ease.OutQuad)
-        .OnComplete(() =>
-        {
-            textComponent.transform.DOScale(originScale, 0.1f).SetEase(Ease.InQuad);
-        });
     }
 }
