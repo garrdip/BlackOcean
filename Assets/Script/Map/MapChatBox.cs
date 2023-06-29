6e006a4b4f7178e675bfe5fb32e8160e3e7e4b73
wnsvy223 <wnsvy223@naver.com>
2023-06-29 16:55:34 +0900

    스크립트 네이밍 변경
    Change-Id: Ic76d38d74230f3582ceb57b35c396aa39d36a1fc

--- Assets/Script/Map/MapChatBox.cs
+++ Assets/Script/Map/MapChatBox.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class MapSceneChatBox : MonoBehaviour,  IPointerEnterHandler, IPointerExitHandler
+public class MapChatBox : MonoBehaviour,  IPointerEnterHandler, IPointerExitHandler
 {
     // 마우스 포인터가 채팅메시지 박스위에 진입 또는 벗어날때 MapUI 클래스의 isMouseOnChatBox 상태 변수값 변경
 
