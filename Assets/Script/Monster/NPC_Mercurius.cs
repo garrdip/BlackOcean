7bda70342f938a8b8f41499aa489d0cbf0dec713
wnsvy223 <wnsvy223@naver.com>
2023-09-06 15:01:59 +0900

    카드 상점 엔피씨 팝업창 구현(진행중)
    Change-Id: Ib8baadfca670c22b5cc755e9b2f6645123ecf4be

--- Assets/Script/Monster/NPC_Mercurius.cs
+++ Assets/Script/Monster/NPC_Mercurius.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using ProjectD;
 using Mirror;
 
 public class NPC_Mercurius : SpawnedMonster
 {
+    void Start()
+    {
+        // NPC_Mercurius 생성될 때 현재 플레이어의 카드 데이터에서 6개의 랜덤 카드데이터를 추출하여 팝업에 6개의 상점카드 세팅
+        if(transform.parent.GetComponent<TargetObject>().isCloneData){
+            foreach(Card card in M_CardManager.instance.ExtractRandomCards(6)){
+                PopUpUIManager.instance.mercuriusPopUp.GetComponent<MercuriusPopUp>().storeCards.Add(card);
+            }
+        }
+    }
+
     void OnMouseDown()
     {
-        Debug.Log("클릭!");
-        if(M_TurnManager.instance.phase == BattleTurn.NONE_BATTLE_SCENE)
-        {
-            Debug.Log("팝업 시작!");
-            PopUpUIManager.instance.HandleMercuriusPopUp(true);
+        if(!EventSystem.current.IsPointerOverGameObject()){ // NPC_Mercurius가 UI에 가려져 있을 경우(팝업이 활성화 된 경우) 클릭 이벤트 방지
+            if(M_TurnManager.instance.phase == BattleTurn.NONE_BATTLE_SCENE){
+                PopUpUIManager.instance.HandleMercuriusPopUp(true);
+            }
         }
     }
 }
--- Assets/Script/UI/PopUpComponent/MercuriusPopUp.cs
+++ Assets/Script/UI/PopUpComponent/MercuriusPopUp.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class MercuriusPopUp : SingletonD<MercuriusPopUp>
+public class MercuriusPopUp : SingletonD<MercuriusPopUp>, IPointerClickHandler
 {
     public CanvasGroup canvasGroup;
-    public Button backBtn;
+    public GridLayoutGroup gridLayoutGroup;
+    public GameObject frame;
+    public bool isMouseOnFrame = false;
+    public List<Card> storeCards = new List<Card>();
+    public List<GameObject> storeCardObjectList = new List<GameObject>();
 
 
     protected override void Awake()
     {
         PopUpUIManager.instance.onMercuriusPopUpShow += OnMercuriusPopUpShow;
         PopUpUIManager.instance.onMercuriusPopUpHide += OnMercuriusPopUpHide;
-        backBtn.onClick.AddListener(() => OnMercuriusPopUpHide());
     }
 
     void OnDestroy()
@@ -22,19 +26,52 @@ public class MercuriusPopUp : SingletonD<MercuriusPopUp>
         DOTween.Kill(canvasGroup);
     }
 
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(!isMouseOnFrame){
+            PopUpUIManager.instance.HandleMercuriusPopUp(false);
+        }
+    }
+
+    // 상점 카드 정보 리스트 요소 추가
+    private void AddDeckList(List<Card> cards, GridLayoutGroup gridLayoutGroup)
+    {
+        ClearDeckList();
+        foreach(Card card in cards){
+            GameObject cardOnDeck = Instantiate(PopUpUIManager.instance.CardOnDeckPrefab);
+            cardOnDeck.transform.SetParent(gridLayoutGroup.transform);
+            cardOnDeck.transform.localScale = new Vector3(1, 1, 1);
+            cardOnDeck.GetComponent<CardOnDeck>().card = card;
+            storeCardObjectList.Add(cardOnDeck);
+        }
+    }
+
+    // 상점 카드 정보 리스트 요소 제거
+    private void ClearDeckList()
+    {
+        for(int i=storeCardObjectList.Count-1; i >=0; i--){
+            Destroy(storeCardObjectList[i]);
+            storeCardObjectList.RemoveAt(i);
+        }
+    }
+
     // -------------------------------------------------------------------  델리게이트 이벤트 콜백 함수 -------------------------------------------------------------------------- //
 
-    // DeckRemovePopUp 활성화 콜백
+    // MercuriusPopUp 활성화 콜백
     public void OnMercuriusPopUpShow()
     {
         canvasGroup.DOFade(1.0f, 0.5f);
+        GameUIManager.instance.GameUI.gameObject.SetActive(false);
+        AddDeckList(storeCards, gridLayoutGroup);
     }
 
-    // DeckRemovePopUp 비활성화 콜백
+    // MercuriusPopUp 비활성화 콜백
     public void OnMercuriusPopUpHide()
     {
+        GameUIManager.instance.GameUI.gameObject.SetActive(true);
         canvasGroup.DOFade(0.0f, 0.5f).OnComplete(() => {
+            ClearDeckList();
             gameObject.SetActive(false);
         });
-    } 
+    }
 }
