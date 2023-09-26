d16cad90ee9e46876ce08948979a2d1da0322f24
wnsvy223 <wnsvy223@naver.com>
2023-09-26 11:14:37 +0900

    어빌리티 화살표 몸통의 부모 SyncVar 참조값 설정 Spawn함수 이전으로 변경(TEST)
    Change-Id: I557ca9ff62a01759449ca3aa1a5c128d094899b0

--- Assets/Script/Player/GamePlayerDeck.cs
+++ Assets/Script/Player/GamePlayerDeck.cs
@@ -330,8 +330,8 @@ public partial class GamePlayerDeck : NetworkBehaviour
                 M_NetworkRoomManager.spawnPrefabs.Find(prefab => prefab.name.Equals("AbilityArrowNode")),
                 arrowSpawnPosition,
                 Quaternion.identity);
-            NetworkServer.Spawn(arrowNode, connectionToClient);
             arrowNode.GetComponent<AbilityCtrlArrowNode>().abilityCtrlArrow = abilityEmitter.GetComponent<AbilityCtrlArrow>(); // 화살표 몸통에 SyncVar로 선언된 부모 오브젝트(화살표) 참조값 설정
+            NetworkServer.Spawn(arrowNode, connectionToClient);
         }
 
         // 화살표 인디케이터 머리 생성
