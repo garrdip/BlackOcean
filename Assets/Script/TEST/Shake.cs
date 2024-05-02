10f180bbca7fa172ca5a6a1a9b094fe9e8fd700e
wnsvy223 <wnsvy223@naver.com>
2024-05-02 16:07:06 +0900

    카메라 Shake 방향(상하좌우, 좌우)변경 및, 강도 변경 테스트 가능 하도록 수정
    Change-Id: Ic877a5542cdcb686e3abb9f82f941c9f5de8159b

--- Assets/Script/TEST/Shake.cs
+++ Assets/Script/TEST/Shake.cs
@@ -6,24 +6,43 @@ using DG.Tweening;
 public class Shake : MonoBehaviour
 {
     public Camera mainCamera;
+
+    public Vector3 originPosition;
+
+    [Range(1f, 3f)]
     public float shakeStrength;
+
+    [Range(0f, 5f)]
     public float shakeDuration;
+
+    [Range(10f, 20f)]
+    public int vibrato;
+
+    [Range(0f, 180f)]
+    public float randomness;
+
+    public bool isShakeXY = false;
 
     void Start()
     {
         mainCamera = GetComponent<Camera>();
+        originPosition = mainCamera.transform.position;
         shakeStrength = 0.3f;
         shakeDuration = 0.2f;
+        vibrato = 10;
+        randomness = 90f;
     }
 
  
     public void Shaking()
     {
-        Vector3 originalPos = mainCamera.transform.position;
-        mainCamera.transform.DOShakePosition(shakeDuration, shakeStrength);
-        DOVirtual.DelayedCall(shakeDuration, () =>
-        {
-            mainCamera.transform.position = originalPos;
+        if(isShakeXY){
+            mainCamera.transform.DOShakePosition(shakeDuration, new Vector3(shakeStrength, 0f, 0f), vibrato, randomness, false, true);
+        }else{
+            mainCamera.transform.DOShakePosition(shakeDuration, shakeStrength, vibrato, randomness, false, true);
+        }
+        DOVirtual.DelayedCall(shakeDuration, () => { 
+            mainCamera.transform.position = originPosition;
         });
     }
 }
