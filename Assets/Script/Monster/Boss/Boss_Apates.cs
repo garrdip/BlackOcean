282083af78a743a1d4dfac61b7982395895954bb
wnsvy223 <wnsvy223@naver.com>
2024-05-31 16:56:35 +0900

    1. 사운드 매니저에 보스 BGM 추가 2. 아파테스, 게라스 생성시 브금 재생 추가
    Change-Id: I153bbe92f6e122faab2c9bd03ef9ad80121f9924

--- Assets/Script/Monster/Boss/Boss_Apates.cs
+++ Assets/Script/Monster/Boss/Boss_Apates.cs
@@ -5,5 +5,12 @@ using Mirror;
 
 public class Boss_Apates : SpawnedMonster
 {
-    
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        // 아파테스 BGM 재생
+        AudioClip momosBGM = M_SoundManager.instance.bgmClips[BGM_TYPE.Boss].Find((audioClip) => audioClip.name.Equals("Boss_Apates"));
+        M_SoundManager.instance.PlayBGM(momosBGM, MusicTransition.Swift, 1.5f);
+    }
 }
--- Assets/Script/Monster/Boss/Boss_Geras.cs
+++ Assets/Script/Monster/Boss/Boss_Geras.cs
@@ -5,5 +5,12 @@ using Mirror;
 
 public class Boss_Geras : SpawnedMonster
 {
-    
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        // 게라스 BGM 재생
+        AudioClip momosBGM = M_SoundManager.instance.bgmClips[BGM_TYPE.Boss].Find((audioClip) => audioClip.name.Equals("Boss_Geras"));
+        M_SoundManager.instance.PlayBGM(momosBGM, MusicTransition.Swift, 1.5f);
+    }
 }
