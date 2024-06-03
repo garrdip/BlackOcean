3205728255e285272e1ebe77903db7f7b9640faa
wnsvy223 <wnsvy223@naver.com>
2024-06-03 15:36:40 +0900

    몬스터 스크립트 파일 및 폴더 정리
    Change-Id: I9bb03e048bd9d8297058d687a3cf3c6b2e3f0a25

---
+++ Assets/Script/Monster/Elite/Executioner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ProjectD;
+using Mirror;
+
+
+public class Executioner : SpawnedMonster
+{
+    public override IEnumerator DoAction()
+    {
+        return base.DoAction();
+    }
+
+    public override void OnChangedNextTarget(ActionTarget oldVal, ActionTarget newVal)
+    {
+        
+    }
+}
