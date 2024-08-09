2b7a6ddbdee529d0c7acc6d5683a3ac0ccffb194
wnsvy223 <wnsvy223@naver.com>
2024-08-09 11:22:25 +0900

    1. 게임씬의 덱버튼들에 ButtonBase 추가 2. 현재이치와 최대이치 아이콘 생성될떄 스케일 애니매이션 추가
    Change-Id: I228cc9fa8a98eca302054603785e2eb8ea839f6c

--- Assets/Script/Player/GamePlayerDeck_IchiPart.cs
+++ Assets/Script/Player/GamePlayerDeck_IchiPart.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 using ProjectD;
-using TMPro;
+using DG.Tweening;
 
 public partial class GamePlayerDeck : NetworkBehaviour
 {
@@ -58,6 +58,7 @@ public partial class GamePlayerDeck : NetworkBehaviour
     private void CreateCurrentItchIcon(int maxIchiValue)
     {
         for(int i=GameUIManager.instance.currentIchiIcons.Count-1; i >= 0; i--){
+            GameUIManager.instance.currentIchiIcons[i].transform.DOKill();
             Destroy(GameUIManager.instance.currentIchiIcons[i]);
             GameUIManager.instance.currentIchiIcons.RemoveAt(i);
         }
@@ -67,12 +68,18 @@ public partial class GamePlayerDeck : NetworkBehaviour
             costIcon.transform.localScale = Vector3.one;
             GameUIManager.instance.currentIchiIcons.Add(costIcon);
         }
+        // 추가된 이치 아이콘 스케일 애니매이션 실행
+        GameObject newCurrentIchiIcon =  GameUIManager.instance.currentIchiIcons[ GameUIManager.instance.currentIchiIcons.Count - 1];
+        newCurrentIchiIcon.transform.DOScale(2f, 0.5f).OnComplete(() => {
+            newCurrentIchiIcon.transform.DOScale(1f, 0.5f);
+        });
     }
 
     // 최대 이치 아이콘 생성
     private void CreateMaxItchIcon(int maxIchiValue)
     {
         for(int i=GameUIManager.instance.maxIchiIcons.Count-1; i >= 0; i--){
+            GameUIManager.instance.maxIchiIcons[i].transform.DOKill();
             Destroy(GameUIManager.instance.maxIchiIcons[i]);
             GameUIManager.instance.maxIchiIcons.RemoveAt(i);
         }
@@ -82,5 +89,10 @@ public partial class GamePlayerDeck : NetworkBehaviour
             costIcon.transform.localScale = Vector3.one;
             GameUIManager.instance.maxIchiIcons.Add(costIcon);
         }
+        // 추가된 최대 이치 아이콘 스케일 애니매이션 실행
+        GameObject newMaxIchiIcon = GameUIManager.instance.maxIchiIcons[GameUIManager.instance.maxIchiIcons.Count -1];
+        newMaxIchiIcon.transform.DOScale(2f, 0.5f).OnComplete(() => {
+            newMaxIchiIcon.transform.DOScale(1f, 0.5f);
+        });
     }
 }
